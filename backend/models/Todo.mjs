6cfaffe3ca55930071ceea2f92c0ab1d9ad79e72
Your Name <you@example.com>
2023-10-14 10:39:28 -0500

    Updated Schema to set required fields. Added new sensitive data variables. Add additional logging for updating task

--- backend/models/Todo.mjs
+++ backend/models/Todo.mjs
@@ -1,11 +1,23 @@
 import mongoose from 'mongoose';
 
 const TodoSchema = new mongoose.Schema({
-    task: { type: String, required: true },
-    description: { type: String },
-    priority: { type: String, default: "Low" },
+    task: { 
+        type: String, 
+        required: true
+    },
+    description: { 
+        type: String,
+        required: true
+    },
+    priority: { 
+        type: String, 
+        default: "Low",
+        enum: ["Low", "Medium", "High"],
+        required: true
+    },
     completed: { type: Boolean, default: false }
 });
+
 
 const Todo = mongoose.model('Todo', TodoSchema);
 
--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,22 +1,42 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/todos';
+const API_URL = process.env.TODO_API_URL || 'http://localhost:5000/api/todos';
 
 export const getTodos = async () => {
-    const response = await axios.get(API_URL);
-    return response.data;
+    try {
+        const response = await axios.get(API_URL);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching tasks:", error);
+        throw error;
+    }
 };
 
 export const createTodo = async (todo) => {
-    const response = await axios.post(API_URL, todo);
-    return response.data;
+    try {
+        const response = await axios.post(API_URL, todo);
+        return response.data;
+    } catch (error) {
+        console.error("Error creating task:", error);
+        throw error;
+    }
 };
 
 export const updateTodo = async (id, updatedTodo) => {
-    const response = await axios.put(`${API_URL}/${id}`, updatedTodo);
-    return response.data;
+    try {
+        const response = await axios.put(`${API_URL}/${id}`, updatedTodo);
+        return response.data;
+    } catch (error) {
+        console.error("Error updating task:", error);
+        throw error;
+    }
 };
 
 export const deleteTodo = async (id) => {
-    await axios.delete(`${API_URL}/${id}`);
+    try {
+        await axios.delete(`${API_URL}/${id}`);
+    } catch (error) {
+        console.error("Error deleting task:", error);
+        throw error;
+    }
 };
