dcd94f28f7b69a84a541837bbe969ca25da1a1a4
Your Name <you@example.com>
2023-10-14 21:29:56 -0500

    Adjusted TaskItem edit field to match styling

--- frontend/src/components/TaskItem.js
+++ frontend/src/components/TaskItem.js
@@ -7,8 +7,6 @@ function TaskItem({ task, onSave, onDelete }) {
     const [errors, setErrors] = useState({});
 
     const handleSave = () => {
-        console.log("Attempting to update task", newTask);
-        
         let errorObj = {};
         if (!newTask.task) errorObj.task = "Task is required!";
         if (!newTask.description) errorObj.description = "Description is required!";
@@ -22,13 +20,11 @@ function TaskItem({ task, onSave, onDelete }) {
         setIsEditing(false);
     };
 
-    const priorityValue = ["Low", "Medium", "High"].includes(newTask.priority) ? newTask.priority : "Low";
-
     return (
         <div className="bg-white p-4 rounded shadow">
             {isEditing ? (
-                <div className="flex space-x-2">
-                    <div className="flex flex-col flex-grow">
+                <div className="flex flex-wrap space-x-2 items-center">
+                    <div className="flex flex-col w-full md:w-1/3 mb-2 md:mb-0">
                         {errors.task && <p className="text-red-500 text-xs mb-1">{errors.task}</p>}
                         <input
                             type="text"
@@ -39,7 +35,7 @@ function TaskItem({ task, onSave, onDelete }) {
                         />
                     </div>
                     
-                    <div className="flex flex-col flex-grow">
+                    <div className="flex flex-col w-full md:w-1/3 mb-2 md:mb-0">
                         {errors.description && <p className="text-red-500 text-xs mb-1">{errors.description}</p>}
                         <input
                             type="text"
@@ -49,12 +45,13 @@ function TaskItem({ task, onSave, onDelete }) {
                             className={`px-3 py-2 border ${errors.description ? 'border-red-500' : 'border-gray-300'} rounded-md w-full`}
                         />
                     </div>
-                    
-                    <div className="flex flex-col">
+
+                    <div className="flex flex-col w-full md:w-1/6 mb-2 md:mb-0">
                         <select
-                            value={priorityValue}
+                            value={newTask.priority}
                             onChange={(e) => setNewTask({ ...newTask, priority: e.target.value })}
                             className={`px-3 py-2 border ${errors.priority ? 'border-red-500' : 'border-gray-300'} rounded-md w-full`}
+                            style={{ height: '42px' }}
                         >
                             <option value="Low">Low</option>
                             <option value="Medium">Medium</option>
@@ -62,24 +59,20 @@ function TaskItem({ task, onSave, onDelete }) {
                         </select>
                     </div>
                     
-                    <div className="flex flex-col justify-center">
-                        <button onClick={handleSave} className="px-4 py-2 bg-blue-500 text-white rounded-md">
+                    <div className="flex flex-col w-full md:w-auto">
+                        <button onClick={handleSave} className="px-4 py-2 bg-blue-500 text-white rounded-md w-full" style={{ height: '42px' }}>
                             Save
                         </button>
                     </div>
                 </div>
             ) : (
-                <div className="flex items-center justify-between">
+                <div className="flex justify-between items-center">
                     <div>
-                        <strong>{task.task}</strong> - {task.description} (Priority: {task.priority})
+                        <strong>{task.task}</strong> - {task.description} - {task.priority}
                     </div>
                     <div>
-                        <button onClick={() => setIsEditing(true)} className="px-4 py-2 bg-blue-500 text-white rounded-md mr-2">
-                            Edit
-                        </button>
-                        <button onClick={() => onDelete(task._id)} className="px-4 py-2 bg-red-500 text-white rounded-md">
-                            Delete
-                        </button>
+                        <button onClick={() => setIsEditing(true)} className="bg-blue-500 text-white px-2 py-1 rounded mr-2">Edit</button>
+                        <button onClick={() => onDelete(task.id)} className="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
                     </div>
                 </div>
             )}
