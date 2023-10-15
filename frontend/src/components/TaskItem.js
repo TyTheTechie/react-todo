d497dd0a680b2fb57ac70e0100d42cec1ad4c283
Your Name <you@example.com>
2023-10-14 19:22:30 -0500

    small tweak

--- frontend/src/components/TaskItem.js
+++ frontend/src/components/TaskItem.js
@@ -49,11 +49,11 @@ function TaskItem({ task, onSave, onDelete }) {
                         />
                     </div>
                     
-                    <div className="flex flex-col">
+                    <div className="flex flex-col flex-grow">
                         <Select
                             value={priorityValue}
                             onChange={(e) => setNewTask({ ...newTask, priority: e.target.value })}
-                            style={{ minWidth: '120px' }}
+                            fullWidth
                         >
                             <MenuItem value="Low">Low</MenuItem>
                             <MenuItem value="Medium">Medium</MenuItem>
